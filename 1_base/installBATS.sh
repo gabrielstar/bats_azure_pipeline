3e11b1ebb060724a18d552b1757134e49f045417
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-05-21 15:21:22 +0200

    add bats tests

--- 1_base/installBATS.sh
+++ 1_base/installBATS.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 git clone https://github.com/ztombol/bats-core $HOME/test/test_helper/batscore
 git clone https://github.com/ztombol/bats-assert $HOME/test/test_helper/batsassert
+ls $HOME/test/test_helper/batscore
 source $HOME/test/test_helper/batscore/script/install-bats.sh ~
 bats -version
